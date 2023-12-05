17f3260f5da66f083c8907fe9856e83b5653106f
August Lukens <august@lukens.co>
2023-12-05 01:39:16 -0500

    Day 3 Star 2 - Welp

--- day3/day3.rb
+++ day3/day3.rb
@@ -112,7 +112,7 @@ if __FILE__ == $0
   input = File.readlines(file_path, chomp: true).map(&:chars)
 
   star_1 = find_valid_numbers(Grid.new(input)).sum
-  star_2 = nil
+  star_2 = "You know, life's too short to worry about some things."
 
   puts "Star 1: #{star_1}"
   puts "Star 2: #{star_2}"
