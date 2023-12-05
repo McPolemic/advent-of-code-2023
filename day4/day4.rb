81b8b97977c23421dc83a76032baf8016bf48a84
August Lukens <august@lukens.co>
2023-12-05 01:57:48 -0500

    Refactor

--- day4/day4.rb
+++ day4/day4.rb
@@ -1,24 +1,20 @@
-def score_card(card)
-  winning_numbers, my_numbers = card
+def score_card_star_1(line)
+  cards = line.split(": ").last
+  winning_numbers, my_numbers = cards
+    .split(" | ")
+    .map{ |side| side.split(" ") }
+
   winning_amount = my_numbers.intersection(winning_numbers).length
 
   return 0 if winning_amount == 0
   2 ** (winning_amount - 1)
 end
 
-def prepare_input(line)
-  cards = line.split(": ").last
-  winning_numbers, my_numbers = cards
-    .split(" | ")
-    .map{ |side| side.split(" ") }
-end
-
 if __FILE__ == $0
   file_path = File.join(File.dirname(__FILE__), 'day4.txt')
   input = File.readlines(file_path, chomp: true)
-  cards = input.map { |line| prepare_input(line) }
 
-  star_1 = cards.map{ |card| score_card(card) }.sum
+  star_1 = input.map { |card| score_card_star_1(card) }.sum
   star_2 = nil
 
   puts "Star 1: #{star_1}"
