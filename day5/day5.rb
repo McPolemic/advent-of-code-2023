003f661e867339e2b19fc7a3d9d77cedd6d8b864
August Lukens <august@lukens.co>
2023-12-06 01:41:58 -0500

    Day 5 Star 1

--- day5/day5.rb
+++ day5/day5.rb
@@ -1,10 +1,113 @@
+require 'pry'
+require 'pry-nav'
+
+class Almanac
+  attr_reader :seeds
+
+  def initialize
+    @library = {}
+    @seeds = []
+  end
+
+  def add_seeds(seeds)
+    @seeds = seeds
+    puts "Added seeds #{seeds.inspect}"
+  end
+
+  def add_map(from, to)
+    @library[[from, to]] = Map.new
+    puts "Added map #{from}->#{to}"
+  end
+
+  def [](from, to)
+    @library[[from, to]]
+  end
+
+  # Were I smarter or it wasn't 1:00 AM, I'd give it a
+  # `from` and a final `to` and have it search through maps
+  # to find a path between the two. I'm just going to
+  # manually send in the list, though.
+  def pipeline(list_of_maps, source)
+    list_of_maps.reduce(source) do |acc, (from, to)|
+      self[from, to].translate(acc)
+    end
+  end
+end
+
+class Map
+  def initialize
+    @rules = []
+  end
+
+  def add_rule(source_start, dest_start, range)
+    source_range = source_start..source_start+range
+    dest_range = dest_start..dest_start+range
+
+    @rules << [source_range, dest_range]
+  end
+
+  def translate(source)
+    rule = @rules.find { |rule| rule.first.include? source }
+    if rule
+      offset = source - rule.first.begin
+      rule.last.begin + offset
+    else
+      source
+    end
+  end
+end
+
+def parse_input(input)
+  from, to = nil
+  buffer = []
+  almanac = Almanac.new
+
+  input.each do |line|
+    case line
+    when /^seeds: ([\d ]+)/
+      seeds = Regexp.last_match
+        .captures
+        .first
+        .split(" ")
+        .map(&:to_i)
+      almanac.add_seeds seeds
+    when /^([a-z]+)-to-([a-z]+) map:/
+      from, to = Regexp.last_match.captures
+      almanac.add_map(from, to)
+    when /^(\d+) (\d+) (\d+)/
+      dest, source, range = Regexp.last_match
+        .captures
+        .map(&:to_i)
+      almanac[from, to].add_rule(source, dest, range)
+    end
+  end
+
+  almanac
+end
+
+def star_1(almanac)
+  pipeline_maps = [
+    %w(seed soil),
+    %w(soil fertilizer),
+    %w(fertilizer water),
+    %w(water light),
+    %w(light temperature),
+    %w(temperature humidity),
+    %w(humidity location)
+  ]
+
+  # Translate all seeds to locations and return the lowest location's seed
+  almanac.seeds.map do |seed|
+    almanac.pipeline(pipeline_maps, seed)
+  end.min
+end
 
 if __FILE__ == $0
   file_path = File.join(File.dirname(__FILE__), 'day5.txt')
-  input = File.readlines(file_path, chomp: true).map(&:chars)
+  input = parse_input(File.readlines(file_path, chomp: true))
 
-  star_1 = find_valid_numbers(Grid.new(input)).sum
-  star_2 = "You know, life's too short to worry about some things."
+  star_1 = star_1(input)
+  star_2 = nil
 
   puts "Star 1: #{star_1}"
   puts "Star 2: #{star_2}"
